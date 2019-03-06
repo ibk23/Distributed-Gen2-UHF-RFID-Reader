90c7f5e1e326eaa1b8977c8c923b288acf52a866
Carl Adams-Waite <cadamswaite@gmail.com>
2019-03-06 16:12:38 +0000

    Working CW transmitter

--- gr-rfid/plotters/plot_3d_from_csv.py
+++ gr-rfid/plotters/plot_3d_from_csv.py
@@ -5,22 +5,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
+x_col=8
+y_col=13
+z_col=11
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-with open('../misc/data/RN16ForMatlab.csv') as csvfile:
+with open('../misc/data/MonzaX-CW.csv') as csvfile:
     reader = csv.reader(csvfile)
     headers = next(reader, None)
     for row in reader:
         print(row)
         try:
-            ax.scatter(float(row[2]), float(row[3]), float(row[1]), c='r', marker='o')
+            ax.scatter(float(row[x_col]), float(row[y_col]), float(row[z_col]), c='r', marker='o')
         except Exception as e:
             print("Could not print row due to",e)
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+ax.set_xlabel(headers[x_col])
+ax.set_ylabel(headers[y_col])
+ax.set_zlabel(headers[z_col])
 
+print(headers)
 plt.show()
